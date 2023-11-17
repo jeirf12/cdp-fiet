a639cd23ef7cc935f421b87cb1f9c8aadfbb1bd7
mateok13 <mateoroserok13@gmail.com>
2023-11-16 23:16:26 -0500

    arreglos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into docentes (id_persona, apellidos, nombres, numero_identificacion, tipo_identificacion, correo, departamento, vinculacion) values (1, "Gironza Pino", "Adrian Felipe", "231141234", "CC", "adrian@asae.co", "sistemas", "vinculacion 1")
+insert into docentes (id_persona, apellidos, nombres, numero_identificacion, tipo_identificacion, correo, departamento, vinculacion) values (2, "Ruiz Figueroa", "Jhonfer", "843743834", "CC", "jhonfer@asae.co", "sistemas", "vinculacion 2")
+insert into docentes (id_persona, apellidos, nombres, numero_identificacion, tipo_identificacion, correo, departamento, vinculacion) values (3, "Rosero Cortes", "Jhonny Mateo", "2311434534", "CC", "jhonny@asae.co", "electronica", "vinculacion 3")
+insert into docentes (id_persona, apellidos, nombres, numero_identificacion, tipo_identificacion, correo, departamento, vinculacion) values (4, "Velasco Mosquera", "Jesus Alberto", "87362371234", "CC", "jesus@asae.co", "electronica", "vinculacion 4")
+
+insert into tipos (id_tipo, nombre) values (1, "revista")
+insert into tipos (id_tipo, nombre) values (2, "periodico")
+insert into tipos (id_tipo, nombre) values (3, "pagina web")
