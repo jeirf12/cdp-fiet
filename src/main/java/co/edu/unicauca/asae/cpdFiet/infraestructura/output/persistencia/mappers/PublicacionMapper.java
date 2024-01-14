450afed8c337c7fc87211e9aba7acbfd5239d85b
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2024-01-14 11:37:09 -0500

    added requirements

--- src/main/java/co/edu/unicauca/asae/cpdFiet/infraestructura/output/persistencia/mappers/PublicacionMapper.java
+++ src/main/java/co/edu/unicauca/asae/cpdFiet/infraestructura/output/persistencia/mappers/PublicacionMapper.java
@@ -1,8 +1,14 @@
 package co.edu.unicauca.asae.cpdFiet.infraestructura.output.persistencia.mappers;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import co.edu.unicauca.asae.cpdFiet.dominio.modelos.Docente;
+import co.edu.unicauca.asae.cpdFiet.dominio.modelos.Publicacion;
+import co.edu.unicauca.asae.cpdFiet.infraestructura.output.persistencia.entidades.DocenteEntity;
+import co.edu.unicauca.asae.cpdFiet.infraestructura.output.persistencia.entidades.PublicacionEntity;
 
 @Configuration
 public class PublicacionMapper {
@@ -10,6 +16,10 @@ public class PublicacionMapper {
   @Bean
   public ModelMapper getModelMapperPublicacion() {
     ModelMapper objMapper = new ModelMapper();
+    TypeMap<PublicacionEntity, Publicacion> mapaPublicacion = objMapper.emptyTypeMap(PublicacionEntity.class, Publicacion.class);
+    mapaPublicacion.addMappings(n -> n.skip(Publicacion::setObjTipo)).implicitMappings();
+    TypeMap<DocenteEntity, Docente> mapaDocente = objMapper.emptyTypeMap(DocenteEntity.class, Docente.class);
+    mapaDocente.addMappings(n -> n.skip(Docente::setObjDireccion)).implicitMappings();
     return objMapper;
   }
 }
